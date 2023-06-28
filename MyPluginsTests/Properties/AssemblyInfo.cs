5780de84d4622ed8b01baa0a24ff7f4bc771eb12
Domt301 <leonardod.trimarchi@gmail.com>
2023-06-28 16:58:24 -0400

    added unit tests

---
+++ MyPluginsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyPluginsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyPluginsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("306a0c02-e2ea-4547-aa06-91086bb7517e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyPluginsTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyPlugins;
+using System;
+
+namespace MyPluginsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanAddTwoNumbers()
+        {
+            // Arrange
+            var x = 1;
+            var y = 2;
+            var expected = 3;
+            AddNumbersClass addnumbersClass = new AddNumbersClass();
+
+
+            // Act
+            var actual = addnumbersClass.AddTwoNumbers(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanSubtractTwoNumbers()
+        {
+            // Arrange
+            var x = 1;
+            var y = 2;
+            var expected = -1;
+            SubtractNumbersClass subtractnumbersClass = new SubtractNumbersClass();
+
+            var actual = subtractnumbersClass.SubtractTwoNumbers(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
